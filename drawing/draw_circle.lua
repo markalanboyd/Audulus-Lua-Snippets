582f485fe656f9c7d1d95f1965e896aa690fb0e7
Mark Boyd <markalanboyd@gmail.com>
2022-09-24 16:10:07 +0200

    Update draw_circle.lua

--- drawing/draw_circle.lua
+++ drawing/draw_circle.lua
@@ -6,6 +6,7 @@
 -- You can change the size of the circle by changing the radius value.
 -- The radius value must be a non-zero positive number.
 -- The radius value can have a decimal, like radius = 1.5
+-- The origin point is the center of the circle.
 
 -- :::: Version History ::::
 -- 1.0 - 2022.09.24 - Created
