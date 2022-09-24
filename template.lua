41e2dcf344630bdd4b98b20f4c639f0f902d6c22
Mark Boyd <markalanboyd@gmail.com>
2022-09-24 13:04:26 +0200

    Create template.lua

---
+++ template.lua
@@ -0,0 +1,11 @@
+-- :::: NAME OF SNIPPET ::::
+-- Description
+
+-- :::: How to Use ::::
+-- Remember to note any variables declared in inspector window!
+
+-- :::: Version History ::::
+-- 1.0 - 202X.XX.XX - Created
+
+
+begin
