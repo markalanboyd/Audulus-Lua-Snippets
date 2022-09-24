64e6abd204a5da5c14155f15ccfb3ce55b97e4d9
Mark Boyd <markalanboyd@gmail.com>
2022-09-24 13:10:55 +0200

    Update template.lua

--- template.lua
+++ template.lua
@@ -2,7 +2,7 @@
 -- Description
 
 -- :::: How to Use ::::
--- Remember to note any variables declared in inspector window!
+-- Remember to note any variables declared in inspector panel!
 
 -- :::: Version History ::::
 -- 1.0 - 202X.XX.XX - Created
